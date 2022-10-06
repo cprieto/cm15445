6253093d24866623a56a98df3580b098c5b076f8
Cristian Prieto <me@cprieto.com>
2022-10-06 19:13:51 +0200

    Add exercise 8

---
+++ hw01/placeholder/q8_kidman_colleagues.sql
@@ -0,0 +1,21 @@
+-- Worked in the same movie as Kidman
+WITH colleagues AS (
+    SELECT DISTINCT person_id
+    FROM crew
+    WHERE title_id IN (
+        -- movies with nicole kidman as actress
+        SELECT title_id
+        FROM crew WHERE person_id = (
+            -- nicole kidman
+            SELECT person_id
+            FROM people
+            WHERE name LIKE '%kidman%'
+              AND born = 1967
+        ) AND category = 'actress'
+    ) AND category IN ('actress', 'actor')
+)
+SELECT p.name
+FROM people p
+JOIN colleagues c
+ON c.person_id = p.person_id
+ORDER BY name;
